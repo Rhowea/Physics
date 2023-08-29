6f17e309e68a181516abd191b6ac49dcd24ff721
Adam Heydenreich <adam.heydenreich@gmail.com>
2023-08-29 15:13:19 +0200

    Added stuff that doesn't work

--- Piece.gd
+++ Piece.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 var impulse_vector = Vector2(0, 100)
-var framecount = 0
+
+func _ready():
+	contact_monitor = true
 
 func _input(_event):
 	#"Wakes" the object from sleep upon detecting an input
@@ -9,11 +11,21 @@ func _input(_event):
 	sleeping = false
 
 func _integrate_forces(state):
-	if Input.is_action_pressed("ui_up"):
-		state.apply_central_impulse(-1 * impulse_vector.rotated(rotation))
-	if Input.is_action_pressed("ui_down"):
-		state.apply_central_impulse(impulse_vector.rotated(rotation))
-	if Input.is_action_pressed("ui_left"):
-		state.apply_torque_impulse(-500)
-	if Input.is_action_pressed("ui_right"):
-		state.apply_torque_impulse(500)
+	var mouse_pos = get_viewport().get_mouse_position()
+	var direction = mouse_pos - position
+	var angle_to = -transform.y.angle_to(direction)
+	state.apply_torque_impulse(500 * sign(angle_to) * min(PI, abs(angle_to)))
+	state.apply_central_impulse(-impulse_vector.rotated(rotation))
+	
+	
+#	if Input.is_action_pressed("ui_up"):
+#		state.apply_central_impulse(-1 * impulse_vector.rotated(rotation))
+#	if Input.is_action_pressed("ui_down"):
+#		state.apply_central_impulse(impulse_vector.rotated(rotation))
+#	if Input.is_action_pressed("ui_left"):
+#		state.apply_torque_impulse(-500)
+#	if Input.is_action_pressed("ui_right"):
+#		state.apply_torque_impulse(500)
+
+func _physics_process(delta):
+	pass
